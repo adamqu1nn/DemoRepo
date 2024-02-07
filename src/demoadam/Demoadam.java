89814b102c19d0217821345559f53694babc061e
adamd <adamd@LAPTOP-G3EPG8TM>
2024-02-07 14:17:01 +0800

    DemoAdam

--- src/demoadam/Demoadam.java
+++ src/demoadam/Demoadam.java
@@ -14,7 +14,7 @@ public class Demoadam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       project file>right clk>versioning>initialize Repository
     }
     
 }
